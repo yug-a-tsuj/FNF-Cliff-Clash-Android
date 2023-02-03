21e3366f28d785f4632c7f32e11633b315c3e3e6
yug a tsuj2351 <122200067+yug-a-tsuj@users.noreply.github.com>
2023-02-03 15:57:58 +0100

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -194,6 +194,7 @@ class TitleState extends MusicBeatState
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuBG'));
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
 		bg.setGraphicSize(Std.int(bg.width * 0.6));
+                bg.screenCenter();
 		bg.updateHitbox();
 		add(bg);
 
@@ -202,17 +203,19 @@ class TitleState extends MusicBeatState
 		logoBl.antialiasing = ClientPrefs.globalAntialiasing;
 		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24);
 		logoBl.animation.play('bump');
-		logoBl.updateHitbox();
+                /*bruh
+                logoBl.y -= 80.5;*/
+		logoBl.updateHitbox(XY);
 		logoBl.screenCenter();
 		// logoBl.color = FlxColor.BLACK;
 
-		swagShader = new ColorSwap();
+		/*swagShader = new ColorSwap();
 		if(!FlxG.save.data.psykaEasterEgg || !easterEggEnabled) {
 			gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
 			gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 			gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 			gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-		}
+		}*/
 		else //Psyka easter egg
 		{
 			gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.04);
